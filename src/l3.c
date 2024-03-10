23abce073fdf6f0008b55b68e013bf04a7d03dfa
Aditya P. Gurajada <adityagurajada@yahoo.com>
2024-03-10 16:47:01 -0700

    [LOC] Integrate with LOC package for improved L3 diagnostic
    This commit integrates with Line-of-Code (LOC) diagnostic
    tooling, which provides a way to encode the code-location where
    the L3 logging interface is invoked as a 4-byte int.
    See https://github.com/Soft-Where-Inc/LineOfCode, which now
    becomes a git-submodule of L3 repo.
    
    - Prev commit [ @SHA 8283e76 ] has already dropped-in LOC-field
      as the `struct l3_entry{}.loc_t loc` field definition.
      The presence of this field in the structure and all L3 APIs
      is protected by `#ifdef L3_LOC_ENABLED` guard.
    
    - In this commit, Makefile is enhanced to invoke the build of
      L3 sample programs under the L3_LOC_ENABLED.
    
    - Use of LOC requires running a Python generator script that comes
      as part of the LOC package. Makefile is changed to handle
      the naming and generation of source headers & file, specific
      to each sample program being built.
    
    - Update l3_dump.py to unpack log-entry struct contents. For now,
      we just unpack the `loc` ID value as-is. (Future commits will
      decode this value to filename / line# pair.)
    
    - Update test.sh and CI build.yml to build-and-test all 3
      sample programs with default L3 mode (LOC OFF) and with
      LOC-enabled. Update `make help` info-messages accordingly.
      Add .gitignore file to ignore LOC-generated artifacts etc.
    
    - Address review comments:
      - Rely on mmap() returning zero-filled memory.
      - [Doc] Add steps to init/update LineOfCode as submodule.

--- src/l3.c
+++ src/l3.c
@@ -20,12 +20,21 @@
 #include <sys/wait.h>
 #include <sys/syscall.h>
 #include <threads.h>
+#include <string.h>
+#include <assert.h>
 
 #include "l3.h"
 
 #ifdef L3_LOC_ENABLED
 #include "loc.h"
 #endif  // L3_LOC_ENABLED
+
+// Define version of static assertion checking that works for both gcc and g++
+#ifdef __cplusplus
+#define L3_STATIC_ASSERT    static_assert
+#else
+#define L3_STATIC_ASSERT    _Static_assert
+#endif
 
 /**
  * L3 Log entry Structure definitions:
@@ -35,11 +44,22 @@ struct l3_entry
     pid_t       tid;
 #ifdef L3_LOC_ENABLED
     loc_t       loc;
+#else
+    uint32_t    loc;
 #endif  // L3_LOC_ENABLED
     const char *msg;
     uint64_t    arg1;
     uint64_t    arg2;
 };
+
+/**
+ * Cross-check LOC's data structures. We need this to be true to ensure
+ * that the sizeof(l3_entry) is unchanged w/LOC ON or OFF.
+ */
+#ifdef L3_LOC_ENABLED
+L3_STATIC_ASSERT(sizeof(loc_t) == sizeof(uint32_t),
+                 "Expected sizeof(loc_t) == 4 bytes.");
+#endif  // L3_LOC_ENABLED
 
 /**
  * L3 Log Structure definitions:
@@ -81,6 +101,8 @@ l3_init(const char *path)
         return -1;
     }
 
+    // Technically, this is not needed as mmap() is guaranteed to return
+    // zero-filled pages. We do this just to be clear where the idx begins.
     l3_log->idx = 0;
 
     /* Let's find where rodata is loaded. */
@@ -112,9 +134,15 @@ l3__log_simple(uint32_t loc, const char *msg, const uint64_t arg1, const uint64_
 {
     int idx = __sync_fetch_and_add(&l3_log->idx, 1) % L3_MAX_SLOTS;
     l3_log->slots[idx].tid = l3_mytid();
+
 #ifdef L3_LOC_ENABLED
     l3_log->slots[idx].loc = (loc_t) loc;
+#else
+#ifdef DEBUG
+    assert(l3_log->slots[idx].loc == 0);
+#endif  // DEBUG
 #endif
+
     l3_log->slots[idx].msg = msg;
     l3_log->slots[idx].arg1 = arg1;
     l3_log->slots[idx].arg2 = arg2;
