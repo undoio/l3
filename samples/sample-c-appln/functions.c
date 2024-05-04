9069e286cbafb247389d0999927c37a986ce4ed8
Aditya P. Gurajada <adityagurajada@yahoo.com>
2024-05-04 02:44:28 -0700

    C-Samples: Prepare sources for inclusion of L3-logging APIs
    - Makefile - specify CC rules and define INCLUDE dir-path.
      Cleanup Makefile spacing, indentations. Add comments.
    - #include stdio.h, stdlib.h, errno.h which are unrelated to L3.
    - Add __FILE__, __LINE__, __func__ to printf().

--- samples/sample-c-appln/functions.c
+++ samples/sample-c-appln/functions.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
 
 void call_function() {
-  printf("Hello World!\n");
+  printf("\n%s:%d::%s() Hello World!\n", __FILE__, __LINE__, __func__);
 }
--- samples/sample-c-appln/main.c
+++ samples/sample-c-appln/main.c
@@ -1,6 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h> // For exit(), EXIT_FAILURE etc.
+#include <errno.h>  // For errno
+
 #include "functions.h"
 
 int main(int argc, char ** argv) {
+  printf("\n%s:%d::%s() Starting ...\n", __FILE__, __LINE__, __func__);
+
   call_function();
-  return 0;
+
+  printf("\n%s:%d::%s() Completed.\n", __FILE__, __LINE__, __func__);
+  return EXIT_SUCCESS;
 }
