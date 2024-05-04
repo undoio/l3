6eb388f190fc699c871efba6dce49cb572ac86cf
Aditya P. Gurajada <adityagurajada@yahoo.com>
2024-05-04 02:55:01 -0700

    C-Samples: Add L3-logging calls to sources; Makefile changes.
    This commit shows the diffs needed to integrate basic L3-logging APIs
    with C-sample program sources.
    
    - Makefile: Build rules to include l3.c in sources to compile
      Use symbol L3_SRC for l3.c file.
    - main.c: Initiate L3-logging by a call to l3_init()
    - functions.c: Invoke l3_log_simple() API (slow logging interface)
    
    Entries in L3 log-file /tmp/c-sample-test.dat can be dumped using:
    
    $ l3_dump.py --log-file /tmp/c-sample-test.dat --binary ./c-sample

--- samples/sample-c-appln/functions.c
+++ samples/sample-c-appln/functions.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
+#include "l3.h"
 
 void call_function() {
   printf("\n%s:%d::%s() Hello World!\n", __FILE__, __LINE__, __func__);
+
+  l3_log_simple("Hello World with L3-logging, addr=%p, size=%d bytes",
+                0xdeadbeef, 42);
 }
