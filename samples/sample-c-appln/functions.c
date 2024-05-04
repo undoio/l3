bce59a5d041991bf7a1146b00572b35cced73ba0
Aditya P. Gurajada <adityagurajada@yahoo.com>
2024-05-04 03:38:30 -0700

    C-Samples: Changes to integrate default LOC-encoding with L3-logging
    This commit shows the diffs needed to integrate, into the C-sample
    program sources, the default LOC-encoding scheme working silently
    behind-the-scenes of the L3-logging APIs.
    
    - Makefile: Build rules to include LOC-related sources to compile
    - functions.c: Minor change to show how-to use the LOC APIs.
    
    - test.sh and CI build.yml changes to run C-sample program.
    
    - test.sh: Add new test methods to build and run C-sample program
      with default L3-logging, and with L3/LOC enabled.
      - Fix shellcheck errors.
    
    - Fix build issues on Mac/OSX.
      - Generate LOC-decoder binary in current dir and mv it over.
      - clean will also `rm` target binary.
    
    - CI/build.yml: Add test calls
    
    Entries in L3 log-file /tmp/c-sample-test.dat can be dumped using:
    
    $ make clean && L3_LOC_ENABLED=1 make c-sample
    
    $ l3_dump.py --log-file /tmp/c-sample-test.dat --binary ./c-sample
    
    You should see this enhanced log-dump output, listing the source-code
    location information where the L3-logging was done.
    
    tid=109035 ./functions.c:11  'Hello World with L3-logging, addr=0xdeadbeef, size=42 bytes'
    
    NOTE: The fragment `./functions.c:11` in above output is new with
          this L3-LOC integration..

--- samples/sample-c-appln/functions.c
+++ samples/sample-c-appln/functions.c
@@ -4,6 +4,12 @@
 void call_function() {
   printf("\n%s:%d::%s() Hello World!\n", __FILE__, __LINE__, __func__);
 
+#ifdef L3_LOC_ENABLED
+  loc_t loc = __LOC__;
+  printf("\nLOC-macros: __LOC__ = %d, LOC_FILE = '%s', LOC_LINE = %d\n",
+         loc, LOC_FILE(loc), LOC_LINE(loc));
+#endif  // L3_LOC_ENABLED
+
   l3_log_simple("Hello World with L3-logging, addr=%p, size=%d bytes",
                 0xdeadbeef, 42);
 
