d99652dc319ef0dfdb8ee451d5ced9549ea6f528
Aditya P. Gurajada <adityagurajada@yahoo.com>
2024-05-03 18:30:20 -0700

    Initial version of SimpleMakefile repo sources.

---
+++ samples/sample-c-appln/functions.h
@@ -0,0 +1,6 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+void call_function();
+
+#endif // FUNCTIONS_H
---
+++ samples/sample-c-appln/main.c
@@ -0,0 +1,6 @@
+#include "functions.h"
+
+int main(int argc, char ** argv) {
+  call_function();
+  return 0;
+}
