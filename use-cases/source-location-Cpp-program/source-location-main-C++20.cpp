c42cbe4562141de07e25ed8b8c69cf1be8bc3336
Aditya P. Gurajada <adityagurajada@yahoo.com>
2024-05-07 06:59:46 -0700

    Minor refactoring of messasges.

--- use-cases/source-location-Cpp-program/source-location-main-C++20.cpp
+++ use-cases/source-location-Cpp-program/source-location-main-C++20.cpp
@@ -10,15 +10,30 @@
 #include <string_view>
 
 void log(const std::string_view message,
-         const std::source_location location =
-               std::source_location::current())
+         const std::source_location location = std::source_location::current())
 {
-    std::clog << "file: "
-              << location.file_name() << '('
+    std::clog << "\n" << location.file_name() << ':'
               << location.line() << ':'
-              << location.column() << ") "
-              << location.function_name() << ": "
-              << message << '\n';
+              << location.column() << "::"
+              << location.function_name()
+              << " [" << sizeof(std::source_location) << "]:"
+              << " [sizeof(file_name)="
+              << sizeof(location.file_name()) << "]: "
+              << "'" << message << "'"
+              << '\n';
+
+    std::source_location curr_location = std::source_location::current();
+
+    std::clog << curr_location.file_name() << ':'
+              << curr_location.line() << ':'
+              << curr_location.column() << "::"
+              << curr_location.function_name()
+              // << " [" << sizeof(std::source_location) << "]: "
+              // << " [" << sizeof(curr_location.file_name()) << "]: "
+              // << message
+              << " [Callee]"
+              << '\n';
+
 }
 
 template<typename T>
@@ -27,8 +42,12 @@ void func(T x)
     log(x);
 }
 
-int main(int, char*[])
+int main(const int argc, char * argv[])
 {
+    std::cout << __func__ << "(): Size of std::source_location: "
+              << sizeof(std::source_location) << " bytes"
+              << std::endl;
+
     log("Hello world!");
     func("Hello C++20!");
 }
