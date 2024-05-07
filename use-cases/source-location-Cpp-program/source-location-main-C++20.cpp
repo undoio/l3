a743d623be3c496b8a00444b458941ee51b3093b
Aditya P. Gurajada <adityagurajada@yahoo.com>
2024-05-07 10:22:40 -0700

    Refactor: Add pr_source_location().

--- use-cases/source-location-Cpp-program/source-location-main-C++20.cpp
+++ use-cases/source-location-Cpp-program/source-location-main-C++20.cpp
@@ -9,11 +9,14 @@
 #include <source_location>
 #include <string_view>
 
+/**
+ * log() - Generic logging method to print source-code-location and a msg.
+ */
 std::source_location
 log(const std::string_view message,
     const std::source_location location = std::source_location::current())
 {
-    std::clog << "\n" << location.file_name() << ':'
+    std::cout << "\n" << location.file_name() << ':'
               << location.line() << ':'
               << location.column() << "::"
               << location.function_name()
@@ -23,22 +26,37 @@ log(const std::string_view message,
               << "'" << message << "'"
               << '\n';
 
+    // Return instance of source_location where this logging happened at.
     std::source_location curr_location = std::source_location::current();
 
     return curr_location;
 
 }
 
+/**
+ * pr_source_location() - Print the contents of a source-code-location object.
+ */
+void
+pr_source_location(const std::source_location loc,
+                   const std::string msg = "")
+{
+    std::cout << loc.file_name() << ':'
+              << loc.line() << ':'
+              << loc.column() << "::"
+              << loc.function_name()
+              << msg
+              << '\n';
+}
+
+/**
+ * func() - Some templatized function to invoke generic log()'ging method.
+ */
 template<typename T>
 void func(T x)
 {
     std::source_location callee = log(x);
-    std::clog << callee.file_name() << ':'
-              << callee.line() << ':'
-              << callee.column() << "::"
-              << callee.function_name()
-              << " [Callee]"
-              << '\n';
+
+    pr_source_location(callee, " [Callee]");
 }
 
 int main(const int argc, char * argv[])
@@ -49,7 +67,7 @@ int main(const int argc, char * argv[])
 
     std::source_location callee = log("Hello world!");
 
-    std::clog << callee.file_name() << ':'
+    std::cout << callee.file_name() << ':'
               << callee.line() << ':'
               << callee.column() << "::"
               << callee.function_name()
