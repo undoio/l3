921d935d18d0a4cfbead80c4a71f39f48ab94632
Aditya Gurajada <gapisback@gmail.com>
2024-06-03 15:16:07 -0700

    Create scripts/setup-ubuntu-linux.sh

---
+++ scripts/setup-ubuntu-linux.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# ##############################################################################
+# l3/scripts/setup-ubuntu-linux.sh:
+# Simple script to setup the required s/w components to get L3 working.
+# ##############################################################################
+
+sudo apt update -y
+
+sudo apt-get install make
+sudo apt-get install gcc g++ -y
+
+sudo apt install -y pylint pip
+sudo apt-get install -y libfmt-dev/jammy
+sudo apt-get install -y libspdlog-dev
+
+pip install pytest prettytable
+
+export PATH="/home/ubuntu/.local/bin:$PATH"
+
