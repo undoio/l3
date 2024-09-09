ffd815cb2893743d4db41b6c3b0d2cc6592c27f1
Greg Law <greg@undo.io>
2024-09-09 21:02:20 +0100

    Remove previously committed debug logging.

--- tests/pytests/l3_dump_test.py
+++ tests/pytests/l3_dump_test.py
@@ -129,7 +129,6 @@ def test_unit_test_dump_log_entries():
                     , 'log-msg: Potential memory overwrite (addr=0xdeadbabe, size=1024)'
                     , 'log-msg: Invalid buffer handle (addr=0xbeefabcd), unused=0'
                    ]
-    print(msg_list)
     assert msg_list == exp_msg_list
 
     exp_arg1_list = [ 1, 3, 10, int('0xdeadbabe', 16), int('0xbeefabcd', 16) ]
@@ -147,7 +146,6 @@ def test_unit_test_dump_log_entries():
     # Unit-tests are currently not enabled to run with LOC_ENABLED env-var
     assert verify_loc_field_is_empty(loc_list) is True
 
-    print(msg_list)
     assert exp_msg_list == msg_list
 
     exp_arg1_list = [ 1, 3, 10, int('0xdeadbabe', 16), int('0xbeefabcd', 16) ]
